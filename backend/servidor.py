fdc0de9a1770cdd5a19609ba41b4a02b030f69f7
JuanAlfonsoDeveloper <jhernandez15007mail.com>
2025-07-31 17:40:05 -0500

    crud de usuarios finalizado

--- backend/servidor.py
+++ backend/servidor.py
@@ -1,3 +1,73 @@
-from usuarios import registrar_usuario
+from usuarios import (
+    registrar_usuario,
+    obtener_usuario,
+    obtener_usuario_por_correo,
+    obtener_usuario_por_telefono,
+    login_usuario,
+    actualizar_usuario,
+    eliminar_usuario
+)
 
-registrar_usuario()
+def menu():
+    while True:
+        print(" --MENU DE USUARIOS-- ")
+        print("1. Registar usuario")
+        print("2. Mostrar todos los usuarios")
+        print("3. Buscar usuario por correo")
+        print("4. Buscar usuario por telefono")
+        print("5. Login de usuario")
+        print("6. Actualizar usuario")
+        print("7. Eliminar usuario")
+        print("8. Salir")
+
+        opcion = input("Elige una opcion: ")
+
+        if opcion == "1":
+            registrar_usuario()
+        
+        elif opcion == "2":
+            obtener_usuario()
+
+        elif opcion == "3":
+            correo = input("Correo a buscar ")
+            usuario = obtener_usuario_por_correo(correo)
+            if usuario: 
+                print("Usuario encontrado:", usuario)
+            else: 
+                print("Usuario no encntrado. ")
+
+
+        elif opcion == "4":
+            telefono = input("Numero de telefono a buscar ")
+            usuario = obtener_usuario_por_telefono(telefono)
+            if usuario: 
+                print("Usuario encontrado:", usuario)
+            else: 
+                print("Usuario no encntrado. ")
+
+        elif opcion == "5":
+            correo = input("Correo: ")
+            contraseña = input("Contraseña: ")
+            login_usuario(correo, contraseña)
+        
+        elif opcion == "6":
+            id_usuario = input("Digie el ID del usuario que desea utilizar: ")
+            nombre = input("Nuevo nombre: ")
+            apellido = input("Nuevo apellido: ")
+            correo = input("Nuevo correo: ")
+            telefono = input("Nuevo telefono: ")
+            direccion = input("Nueva direccion: ")
+            actualizar_usuario(id_usuario, nombre, apellido, correo, telefono, direccion)
+
+        elif opcion == "7":
+            id_usuario = input("Id del usuario a eliminar: ")
+            eliminar_usuario(id_usuario)
+
+        elif opcion == "8":
+            print("Saliendo del sistema ")
+            break
+
+        else:
+            print("Opcion no valida. Intente de nuevo")
+
+menu()
--- backend/usuarios.py
+++ backend/usuarios.py
@@ -1,6 +1,8 @@
 from db import conectar
 import hashlib
 
+#-- REGISTRAR USURIO --
+
 def registrar_usuario():
     print("Registrar Usuario")
 
@@ -39,3 +41,115 @@ def cifrar_contraseña(contraseña):
     sha256 = hashlib.sha256()
     sha256.update(contraseña.encode("utf-8"))
     return sha256.hexdigest()
+
+
+#-- OBTENER USUARIOS --
+def obtener_usuario():
+    conexion = conectar()
+    if not conexion:
+        return
+    try:
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM usuario")
+        usuarios = cursor.fetchall()
+        for usuario in usuarios:
+            print(usuario)
+    except Exception as e:
+        print("Error al obtener usuarios: ", e)
+    finally: 
+        conexion.close()
+
+
+#-- OBTENER USUARIO POR CORREO --
+def obtener_usuario_por_correo(correo):
+    conexion = conectar()
+    if not conexion:
+        return
+    try: 
+        cursor = conexion.cursor()
+        consulta = "SELECT * FROM usuario WHERE correo_usuario = %s"
+        cursor.execute(consulta, (correo,))
+        usuario = cursor.fetchone()
+        return usuario
+    except Exception as e:
+        print("Error al obtener usuario:", e)
+    finally:
+        conexion.close()
+
+#-- OBTENER USUARIO POR TELEFONO --
+def obtener_usuario_por_telefono(telefono):
+    conexion = conectar()
+    if not conexion:
+        return
+    try:
+        cursor = conexion.cursor()
+        consulta = "SELECT * FROM usuario WHERE telefono_usuario = %s"
+        cursor.execute(consulta, (telefono))
+        usuario = cursor.fetchone()
+        return usuario
+    except Exception as e:
+        print("Error al obtener usuario por telefono:", e)
+    finally:
+        conexion.close()
+
+
+#-- VALIDAR LOGIN --
+import hashlib
+
+def login_usuario(correo, contraseña):
+    conexion = conectar()
+    if not conexion:
+        return
+    try:
+        cursor = conexion.cursor()
+        consulta = "SELECT * FROM usuario WHERE correo_usuario = %s AND contraseña_usuario = %s"
+        contraseña_cifrada = hashlib.sha256(contraseña.encode()).hexdigest()
+        cursor.execute(consulta, (correo, contraseña_cifrada))
+        usuario = cursor.fetchone()
+        if usuario:
+            print("Login exitoso")
+            return usuario
+        else: 
+            print("correo o contraseña incorrectos")
+    except Exception as e:
+        print("Error al hacer login:", e)
+    finally: 
+        conexion.close()
+        
+#-- ACTUALIZAR DATOS DE UN USUARIO --
+def actualizar_usuario(id_usuario, nombre, apellido, correo, telefono, direccion):
+    conexion = conectar()
+    if not conexion: 
+        return
+    try:
+        cursor = conexion.cursor()
+        consulta = """
+        UPDATE usuario SET nombre_usuario=%s, apellido_usuario=%s,
+        correo_usuario=%s, telefono_usuario=%s, direccion_usuario=%s
+        WHERE id_usuario=%s
+        """
+        datos = (nombre, apellido, correo, telefono, direccion, id_usuario)
+        cursor.execute(consulta, datos)
+        conexion.commit()
+        print("Usuario actualizado correctamente")
+    except Exception as e:
+        print("Error al actualizar usuario:", e)
+    finally:
+        conexion.close()
+
+#-- ELIMINAR USUARIO --
+
+def eliminar_usuario(id_usuario):
+    conexion = conectar()
+    if not conexion:
+        return
+    try:
+        cursor = conexion.cursor()
+        consulta = "DELETE FROM usuario WHERE id_usuario = %s"
+        cursor.execute(consulta, (id_usuario,))
+        conexion.commit()
+        print("Usuario eliminado correctamente")
+    except Exception as e:
+        print("Error al elminar el usuario:", e)
+    finally:
+        conexion.close()
