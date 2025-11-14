b6956edf38d8c6c241e9bc052dd20db0fc0b451a
JuanAlfonsoDeveloper <juanalfonsowork@gmail.com>
2025-11-14 07:44:36 -0500

    Se adelentaron las funciones de los vendedores faltan usuarios

--- backend/servidor.py
+++ backend/servidor.py
@@ -31,8 +31,8 @@ from carrito import(
 def mostrar_menu_por_rol(usuario):
     id_usuario, nombre_usuario, id_rol = usuario
     while True:
-        print("\n --- MENU PRINCIPAL ADMINISTRADOR ---")
         if id_rol == 1: #Administrador
+            print("\n --- MENU PRINCIPAL ADMINISTRADOR ---")
             print("1. Registrar Usuario: ")
             print("2. Mostrar todos los usuarios: ")
             print("3. Buscar usuario por correo: ")
@@ -47,136 +47,150 @@ def mostrar_menu_por_rol(usuario):
             print("12. Cerrar sesion: ")
             opcion = input("Seleccione una opcion: ")
             
-        #  Opcion 1. Registrar Usuario
-        if opcion == "1":
-            print("--- REGISTRAR USUARIOS ---")
-            nombre = input("Digite su nombre: ").strip()
-            apellido = input("Digite su apellido: ").strip()
-            correo = input("Digite su correo: ").strip()
-            telefono = input("Digite su telefono: ").strip()
-            contraseña = input("Digite su contraseña: ").strip()
-            direccion = input("Digite su direccion: ").strip()
-            id_rol = input("Digite su codigo del rol: ").strip()
-            
-            
-            if not (nombre and apellido and correo and telefono and contraseña and direccion and id_rol):
-                print("X ERROR: Todos los campos son obligatorios. Intente nuevamente")
-            else: 
-                registrar_usuario(nombre, apellido, correo, telefono, contraseña, direccion, id_rol)
-        
-        # Opcion 2. Mostrar todos los usuarios:
-        elif opcion == "2":
-            print("--- MOSTRAR USUARIOS ---")
-            obtener_usuario()
-        
-        # Opcion 3. Buscar usuarios por correo:
-        elif opcion == "3":
-            print("--- BUSCAR USUARIO POR CORREO ---")
-            correo = input("Correo a buscar: ")
-            usuario = obtener_usuario_por_correo(correo)
-            print("Resultado", usuario if usuario else "Usuario no encontrado")
-            
-        # Opcion 4. Buscar usuarios por telefono:
-        elif opcion == "4":
-            print("--- BUSCAR USUARIO POR TELEFONO ---")
-            telefono = input("Telefono a buscar: ")
-            usuario = obtener_usuario_por_telefono(telefono)
-            print("Resultado", usuario if usuario else "Usuario no encontrado")
-            
-        # Opcion 5. Actualizar usuario: 
-        elif opcion == "5":
-            print("--- ACTUALIZAR USUARIO ---")
-            id_usuario = input("ID del usuario a actualizar: ").strip()
-            nombre = input("Nuevo nombre: ").strip()
-            apellido = input("Nuevo apellido: ").strip()
-            correo = input("Nuevo correo: ").strip()
-            telefono = input("Nuevo telefono: ").strip()
-            contraseña = input("Nueva contraseña ").strip()
-            direccion = input("Nueva direccion: ").strip()  
-            if not (id_usuario and nombre and apellido and correo and telefono and contraseña and  direccion  ):
-                print("X ERROR: Todos los campos son obligatorios. Intente nuevamente")
-            else: 
-                actualizar_usuario(id_usuario, nombre, apellido, correo, telefono, contraseña, direccion,)
-        
-        # Opcion 6. Eliminar usuario:
-        elif opcion == "6":
-            id_usuario = input("ID del usuario a eliminar: ")
-            eliminar_usuario(id_usuario)
-            
-        # Opcion 7. Registrar producto:
-        if opcion == "7":
-            registrar_producto()
-            
-        # Opcion 8. Mostrar productos:
-        elif opcion == "8":
-            obtener_productos()
-            
-        # Opcion 9. Buscar productos:
-        elif opcion == "9":
-            nombre = input("Nombre del producto: ")
-            buscar_producto_por_nombre(nombre)
-        
-        # Opcion 10. Actualizar productos:
-        elif opcion == "10":
-            id_producto = input("ID del producto a actualizar: ")
-            titulo = input("Nuevo titulo: ")
-            precio = input("Nuevo precio: ")
-            imagen = input("Nueva ruta de la imagen / URL: ")
-            descripcion = input("Nueva descripcion: ")
-            stock = input("Nuevo stock: ")
-            id_usuario = input("ID del usuario que publica: ")
-            actualizar_productos(id_producto, titulo, precio, imagen, descripcion, stock, id_usuario)
-            
-        # Opcion 11. Eliminar productos:
-        elif opcion == "11":
-            id_producto = input("ID del producto a eliminar: ")
-            eliminar_productos(id_producto)
-        
-        # Opcion 12. Cerrar sesion:
-        elif opcion == "12":
-            print("Cerrando sesion ...")
-            break
-            
-        
-            # if opcion == "1":
-            #     menu_usuario()
-            # elif opcion == "2":
-            #     menu_productos()
-            # elif opcion == "3":
-            #     print("Aqui se ven los reportes...")
-            # 
-            # else: 
-            #     print("Opcion no valida")
-        
-        elif id_rol == 2: #Vendedor
-            print("1. Agregar producto: ")
-            print("2. Actualizar productos: ")
-            print("3. Eliminar producto ")
-            print("4. Ver productos")
-            print("5. Cerrar sesion: ")
-            opcion = input("Seleccione una opcion: ")
-            
+            #  Opcion 1. Registrar Usuario
             if opcion == "1":
-                registrar_producto()
+                print("--- REGISTRAR USUARIOS ---")
+                nombre = input("Digite su nombre: ").strip()
+                apellido = input("Digite su apellido: ").strip()
+                correo = input("Digite su correo: ").strip()
+                telefono = input("Digite su telefono: ").strip()
+                contraseña = input("Digite su contraseña: ").strip()
+                direccion = input("Digite su direccion: ").strip()
+                id_rol = input("Digite su codigo del rol: ").strip()
+                
+                
+                if not (nombre and apellido and correo and telefono and contraseña and direccion and id_rol):
+                    print("X ERROR: Todos los campos son obligatorios. Intente nuevamente")
+                else: 
+                    registrar_usuario(nombre, apellido, correo, telefono, contraseña, direccion, id_rol)
+            
+            # Opcion 2. Mostrar todos los usuarios:
             elif opcion == "2":
+                print("--- MOSTRAR USUARIOS ---")
+                obtener_usuario()
+            
+            # Opcion 3. Buscar usuarios por correo:
+            elif opcion == "3":
+                print("--- BUSCAR USUARIO POR CORREO ---")
+                correo = input("Correo a buscar: ")
+                usuario = obtener_usuario_por_correo(correo)
+                print("Resultado", usuario if usuario else "Usuario no encontrado")
+                
+            # Opcion 4. Buscar usuarios por telefono:
+            elif opcion == "4":
+                print("--- BUSCAR USUARIO POR TELEFONO ---")
+                telefono = input("Telefono a buscar: ")
+                usuario = obtener_usuario_por_telefono(telefono)
+                print("Resultado", usuario if usuario else "Usuario no encontrado")
+                
+            # Opcion 5. Actualizar usuario: 
+            elif opcion == "5":
+                print("--- ACTUALIZAR USUARIO ---")
+                id_usuario = input("ID del usuario a actualizar: ").strip()
+                nombre = input("Nuevo nombre: ").strip()
+                apellido = input("Nuevo apellido: ").strip()
+                correo = input("Nuevo correo: ").strip()
+                telefono = input("Nuevo telefono: ").strip()
+                contraseña = input("Nueva contraseña ").strip()
+                direccion = input("Nueva direccion: ").strip()  
+                if not (id_usuario and nombre and apellido and correo and telefono and contraseña and  direccion  ):
+                    print("X ERROR: Todos los campos son obligatorios. Intente nuevamente")
+                else: 
+                    actualizar_usuario(id_usuario, nombre, apellido, correo, telefono, contraseña, direccion,)
+            
+            # Opcion 6. Eliminar usuario:
+            elif opcion == "6":
+                id_usuario = input("ID del usuario a eliminar: ")
+                eliminar_usuario(id_usuario)
+                
+            # Opcion 7. Registrar producto:
+            elif opcion == "7":
+                registrar_producto()
+                
+            # Opcion 8. Mostrar productos:
+            elif opcion == "8":
+                obtener_productos()
+                
+            # Opcion 9. Buscar productos:
+            elif opcion == "9":
+                nombre = input("Nombre del producto: ")
+                buscar_producto_por_nombre(nombre)
+            
+            # Opcion 10. Actualizar productos:
+            elif opcion == "10":
                 id_producto = input("ID del producto a actualizar: ")
-                titulo = input("Digite el nuevo titulo del producto desea actualizar :")
-                precio = input("Digite el nuevo precio del producto que desea actualizar :")
-                imagen = input("Digite la nueva imagen que desea actualizar :")
-                descripcion = input("Digite la nueva descripcion que desea actualizar :")
-                stock = input("Digite el nuevo stock que desea actualizar :")
+                titulo = input("Nuevo titulo: ")
+                precio = input("Nuevo precio: ")
+                imagen = input("Nueva ruta de la imagen / URL: ")
+                descripcion = input("Nueva descripcion: ")
+                stock = input("Nuevo stock: ")
+                id_usuario = input("ID del usuario que publica: ")
                 actualizar_productos(id_producto, titulo, precio, imagen, descripcion, stock, id_usuario)
                 
-            elif opcion == "3":
+            # Opcion 11. Eliminar productos:
+            elif opcion == "11":
                 id_producto = input("ID del producto a eliminar: ")
                 eliminar_productos(id_producto)
-            elif opcion == "4":
-                obtener_productos()
-            elif opcion == "5":
+            
+            # Opcion 12. Cerrar sesion:
+            elif opcion == "12":
                 print("Cerrando sesion ...")
                 break
-            else: 
-                print("Opcion no valida")
+            # VALIADACION POR PONER DIGITOS QUE NO SON 
+            else:
+                print("Error opcion invalida:")
+            
+        
+        
+        elif id_rol == 2: #Vendedor
+            print("\n --- MENU PRINCIPAL VENDEDORES ---")
+            print("1. Registrar producto: ")
+            print("2. Mostrar productos: ")
+            print("3. Buscar productos: ")
+            print("4. Actualizar productos: ")
+            print("5. Eliminar productos: ")
+            print("6. Cerrar sesion: ")
+            opcion = input("Seleccione una opcion: ")
+            
+            # Opcion 1. Registrar producto:
+            if opcion == "1":
+                registrar_producto()
+                
+            # Opcion 2. Mostrar productos:
+            elif opcion == "2":
+                obtener_productos()
+                
+            # Opcion 3. Buscar productos:
+            elif opcion == "3":
+                nombre = input("Nombre del producto: ")
+                buscar_producto_por_nombre(nombre)
+            
+            # Opcion 4. Actualizar productos:
+            elif opcion == "4":
+                id_producto = input("ID del producto a actualizar: ")
+                titulo = input("Nuevo titulo: ")
+                precio = input("Nuevo precio: ")
+                imagen = input("Nueva ruta de la imagen / URL: ")
+                descripcion = input("Nueva descripcion: ")
+                stock = input("Nuevo stock: ")
+                id_usuario = input("ID del usuario que publica: ")
+                actualizar_productos(id_producto, titulo, precio, imagen, descripcion, stock, id_usuario)
+                
+            # Opcion 5. Eliminar productos:
+            elif opcion == "5":
+                id_producto = input("ID del producto a eliminar: ")
+                eliminar_productos(id_producto)
+            
+            # Opcion 6. Cerrar sesion:
+            elif opcion == "6":
+                print("Cerrando sesion ...")
+                break
+            # VALIADACION POR PONER DIGITOS QUE NO SON 
+            else:
+                print("Error opcion invalida")
+           
+                
+            
                 
         elif id_rol == 3: #Cliente
             print("1. Ver productos : ")
@@ -300,7 +314,7 @@ if __name__ == "__main__":
 
                 
                 
-# ------------------ Se adelentaron las funciones de los administradores faltan los vendedores y usuario -----------------------------------
+# ------------------ Se adelentaron las funciones de los vendedores faltan usuarioa -----------------------------------
 
 
 
