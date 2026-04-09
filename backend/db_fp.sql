96acc5fdf167063aa9ee6b5f7089c00fb7dea011
JuanAlfonsoDeveloper <juanalfonsowork@gmail.com>
2026-04-09 10:53:28 -0500

    --  Se modifico la base de datos para que tuviera el campo de categoria y se va cambiar el diseño para que primero muestre las categorias

--- backend/db_fp.sql
+++ backend/db_fp.sql
@@ -1,11 +1,6 @@
 CREATE DATABASE db_first_proyect;
 
 
-
-
-
-
-
 CREATE TABLE ROL(
 	id_rol INT AUTO_INCREMENT PRIMARY KEY,
     nombre_rol varchar(50) NOT NULL
@@ -26,6 +21,11 @@ CREATE TABLE USUARIO(
     FOREIGN KEY (id_rol) REFERENCES ROL(id_rol)
 );
 
+CREATE TABLE CATEGORIA(
+    id_categoria int AUTO_INCREMENT PRIMARY KEY,
+    nombre_categoria varchar(50) NOT NULL
+);
+
 
 
 CREATE TABLE PRODUCTO(
@@ -36,8 +36,9 @@ CREATE TABLE PRODUCTO(
     descripcion_producto text,
     stock_producto INT NOT NULL DEFAULT 0,
     id_usuario INT,
+    id_categoria INT,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-    
     
 );
 
@@ -58,7 +59,14 @@ INSERT INTO `rol`VALUES (1,'cliente');
 INSERT INTO `rol`VALUES (2,'empleado');
 INSERT INTO `rol`VALUES (3,'cliente');
 
+-- Inserccion de categoria 
+INSERT INTO `categoria`VALUES (null,'Futbol');
+INSERT INTO `categoria`VALUES (null,'Basquet');
+
+-- Inserccion de usuarios 
+INSERT INTO `usuario`VALUES (null,'Juan','Alfonso','j@gmail.com','31234567','1234','calle falsa 123',1);
+
 
 -- Inserccion de productos 
-INSERT INTO `producto` VALUES (1,'Pesas','10000','url','Texto','59',1);
-INSERT INTO `producto` VALUES (2,'Saco de Box','50000','url','Texto','100',2);
+INSERT INTO `producto` VALUES (1,'Balon de Basquet','10000','url','Texto','59',1,1);
+INSERT INTO `producto` VALUES (2,'Balon de futbol','50000','url','Texto','100',1,2);
